123b0ad19fbc39f85e2972e1646aaff3514bf275
kjwon15 <kjwonmail@gmail.com>
2014-04-29 16:50:23 +0900

    start main activity from splash

--- LaunchSelector/src/main/java/kr/ac/dju/launch/SplashActivity.java
+++ LaunchSelector/src/main/java/kr/ac/dju/launch/SplashActivity.java
@@ -1,34 +1,30 @@
 package kr.ac.dju.launch;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
 
-public class SplashActivity extends ActionBarActivity {
+public class SplashActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(this);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.splash, menu);
         return true;
@@ -46,20 +42,11 @@ public class SplashActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_splash, container, false);
-            return rootView;
+    @Override
+    public void onClick(View view) {
+        if (startButton.getId() == view.getId()) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
         }
     }
-
 }
