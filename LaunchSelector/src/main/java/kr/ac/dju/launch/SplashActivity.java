0a3d14beebd9aa94115db98b1457e4f6c25b2f3c
kjwon15 <kjwonmail@gmail.com>
2014-04-30 01:37:04 +0900

    Hide actionbar on lower API

--- LaunchSelector/src/main/java/kr/ac/dju/launch/SplashActivity.java
+++ LaunchSelector/src/main/java/kr/ac/dju/launch/SplashActivity.java
@@ -2,6 +2,7 @@ package kr.ac.dju.launch;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,9 @@ public class SplashActivity extends ActionBarActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
         startButton = (Button) findViewById(R.id.startButton);
         startButton.setOnClickListener(this);
