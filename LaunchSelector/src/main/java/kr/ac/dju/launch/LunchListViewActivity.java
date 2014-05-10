32572d46e669d89c1227977a2c4e9e4f0dbdf257
kjwon15 <kjwonmail@gmail.com>
2014-05-11 04:44:33 +0900

    Preset adapter class.

--- LaunchSelector/src/main/java/kr/ac/dju/launch/LunchListViewActivity.java
+++ LaunchSelector/src/main/java/kr/ac/dju/launch/LunchListViewActivity.java
@@ -1,7 +1,17 @@
 package kr.ac.dju.launch;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import kr.ac.dju.launch.db.Preset;
 
 /**
  * Created by hayan on 14. 5. 11.
@@ -12,5 +22,41 @@ public class LunchListViewActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+    }
+
+    private class PresetAdapter extends BaseAdapter {
+        private Context context;
+        private LayoutInflater inflater;
+        private List<Preset> list;
+
+        public PresetAdapter(Context context, List<Preset> list) {
+            super();
+            this.context = context;
+            this.inflater = LayoutInflater.from(context);
+            this.list = list;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView view = (TextView) inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            Preset preset = list.get(position);
+            view.setText(preset.getName());
+            return view;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
     }
 }
