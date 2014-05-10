323229767a753b660c1301da9b3f80635d794a7e
kjwon15 <kjwonmail@gmail.com>
2014-05-11 06:48:47 +0900

    attach listview.
    Conflicts:
    	LaunchSelector/src/main/java/kr/ac/dju/launch/LunchListViewActivity.java

--- LaunchSelector/src/main/java/kr/ac/dju/launch/LunchListViewActivity.java
+++ LaunchSelector/src/main/java/kr/ac/dju/launch/LunchListViewActivity.java
@@ -1,27 +1,70 @@
 package kr.ac.dju.launch;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
 
+import kr.ac.dju.launch.db.LunchDbAdapter;
 import kr.ac.dju.launch.db.Preset;
 
 /**
  * Created by hayan on 14. 5. 11.
  */
-public class LunchListViewActivity extends Activity {
+public class LunchListViewActivity extends ListActivity implements View.OnClickListener {
+
+    private Button btnOk;
+    private Button btnCancel;
+
+    private LunchDbAdapter dbAdapter;
+    private PresetAdapter presetAdapter;
+    private List<Preset> presetList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lunch_list_view);
 
+        dbAdapter = new LunchDbAdapter(this);
+        setListViewItem();
+        InitControllerObject();
+
+    }
+
+    private void setListViewItem() {
+        presetList = dbAdapter.fetchAllPresets();
+        presetAdapter = new PresetAdapter(this, presetList);
+
+        setListAdapter(presetAdapter);
+    }
+
+    private void invalidateListViewItem() {
+        presetList.clear();
+        for (Preset preset : dbAdapter.fetchAllPresets()) {
+            presetList.add(preset);
+        }
+        presetAdapter.notifyDataSetChanged();
+    }
+
+    private void InitControllerObject() {
+        btnOk = (Button) findViewById(R.id.select_ok);
+        btnCancel = (Button) findViewById(R.id.select_cancel);
+
+        btnOk.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        // TODO : event process for buttons
     }
 
     private class PresetAdapter extends BaseAdapter {
