c733b0832465566b8a3063bfbf7c9efef2500027
kjwon15 <kjwonmail@gmail.com>
2014-05-20 14:39:41 +0900

    Remove unised var.

--- LaunchSelector/src/main/java/kr/ac/dju/launch/C.java
+++ LaunchSelector/src/main/java/kr/ac/dju/launch/C.java
@@ -4,6 +4,5 @@ package kr.ac.dju.launch;
  * Created by kjwon15 on 2014. 4. 30..
  */
 public class C {
-    public static final String EXTRA_NAME_FOODS = "foods";
     public static final String EXTRA_PRESET_ROWID = "preset_rowid";
 }
