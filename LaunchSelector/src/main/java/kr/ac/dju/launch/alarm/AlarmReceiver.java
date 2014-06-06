b96be7ce964b03df3f8b77695e8d6c168b659ec7
kjwon15 <kjwonmail@gmail.com>
2014-06-07 01:09:47 +0900

    Set alarm at exactly at minutes.

--- LaunchSelector/src/main/java/kr/ac/dju/launch/alarm/AlarmReceiver.java
+++ LaunchSelector/src/main/java/kr/ac/dju/launch/alarm/AlarmReceiver.java
@@ -39,6 +39,8 @@ public class AlarmReceiver extends BroadcastReceiver {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, prefs.getInt("notifications_time.hour", 17));
         calendar.set(Calendar.MINUTE, prefs.getInt("notifications_time.minute", 30));
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
 
         if (now.getTimeInMillis() - calendar.getTimeInMillis() > 0) {
             calendar.add(Calendar.DAY_OF_MONTH, 1);
